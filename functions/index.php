3e431bcab97ab7a167fefab293a2eae8e07754e8
Alexander Kaydansky <kaydansky@gmail.com>
2025-11-19 16:07:55 +0300

    Update with suuort of Netlify Functions specific export format.

--- functions/index.php
+++ functions/index.php
@@ -1,39 +1,58 @@
 <?php
-$source = realpath(__DIR__ . '/..'); 
-$target = '/tmp/site'; 
 
-if (!is_dir($target)) {
-    mkdir($target, 0777, true);
-
-    $copy = function ($from, $to) use (&$copy) {
-        foreach (scandir($from) as $item) {
-            if ($item === '.' || $item === '..') continue;
-
-            $src = $from . '/' . $item;
-            $dst = $to . '/' . $item;
-
-            if (is_dir($src)) {
-                mkdir($dst, 0777, true);
-                $copy($src, $dst);
-            } else {
-                copy($src, $dst);
+function handler($event, $context) {
+    $source = realpath(__DIR__ . '/..');
+    $target = '/tmp/site';
+    
+    if (!is_dir($target)) {
+        mkdir($target, 0777, true);
+        
+        $copy = function ($from, $to) use (&$copy) {
+            foreach (scandir($from) as $item) {
+                if ($item === '.' || $item === '..') continue;
+                
+                $src = $from . '/' . $item;
+                $dst = $to . '/' . $item;
+                
+                if (is_dir($src)) {
+                    mkdir($dst, 0777, true);
+                    $copy($src, $dst);
+                } else {
+                    copy($src, $dst);
+                }
             }
-        }
-    };
-
-    $copy($source, $target);
+        };
+        
+        $copy($source, $target);
+    }
+    
+    chdir($target);
+    
+    $_SERVER['REQUEST_METHOD'] = $event['httpMethod'] ?? 'GET';
+    $_SERVER['REQUEST_URI'] = $event['path'] ?? '/';
+    $_SERVER['QUERY_STRING'] = $event['queryStringParameters'] ? http_build_query($event['queryStringParameters']) : '';
+    $_SERVER['SCRIPT_FILENAME'] = $target . '/index.php';
+    $_SERVER['SCRIPT_NAME'] = '/index.php';
+    $_SERVER['PHP_SELF'] = '/index.php';
+    
+    ob_start();
+    
+    try {
+        require $target . '/index.php';
+        $body = ob_get_contents();
+    } catch (Exception $e) {
+        $body = 'Error: ' . $e->getMessage();
+    } finally {
+        ob_end_clean();
+    }
+    
+    return [
+        'statusCode' => 200,
+        'headers' => [
+            'Content-Type' => 'text/html; charset=utf-8'
+        ],
+        'body' => $body
+    ];
 }
 
-chdir($target);
-
-$_SERVER['SCRIPT_FILENAME'] = $target . '/index.php';
-$_SERVER['SCRIPT_NAME']     = '/index.php';
-$_SERVER['PHP_SELF']        = '/index.php';
-$_SERVER['REQUEST_URI']     = parse_url($_SERVER['X_ORIGINAL_URI'] ?? '/', PHP_URL_PATH)
-                            ?: '/';
-
-foreach ($_ENV as $key => $value) {
-    putenv("$key=$value");
-}
-
-require $target . '/index.php';
+return handler($event ?? [], $context ?? []);
